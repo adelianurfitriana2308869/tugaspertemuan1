2c45d43039f0f19200ca627bd17da65a6c9cadbb
Adelia Nur Fitriana <adeliaanf@gmail.com>
2025-02-10 19:30:23 +0700

    Add files via upload

---
+++ Tugas1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas1;
+
+/**
+ *
+ * @author acer
+ */
+public class Tugas1 {
+
+   public static void main(String[] args) {
+        String nama = "Adelia"; 
+        String nim = "2308869"; 
+        
+        // Output
+        System.out.println("Hallo World nama saya " + nama + " dengan NIM " + nim + ", Saya Senang Belajar Java ");
+    }
+    
+}
